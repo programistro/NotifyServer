144e53854b260320b7635cc05b8abe3aa4ab6527
prrogramistro <r.grigorev1502@gmail.com>
2025-04-12 18:33:46 +0300

    users

---
+++ NotifyNet.Core/Dto/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyNet.Core.Dto;
+
+public class UserDto
+{
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+
+    public string Name { get; set; }
+
+    public string LastName { get; set; }
+}
---
+++ NotifyNet.Web/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NotifyNet.Application.Interface;
+using NotifyNet.Core.Dto;
+using NotifyNet.Core.Models;
+
+namespace NotifyNet.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger;
+    private readonly IUserService _userService;
+
+    public AuthController(ILogger<AuthController> logger, IUserService userService)
+    {
+        _logger = logger;
+        _userService = userService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserDto dto)
+    {
+        var user = await _userService.AddAsync(dto);
+
+        if (user == null)
+        {
+            return BadRequest(new { message = "Username or password is incorrect" });
+        }
+        
+        var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Email) };
+        var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromDays(1)),
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),
+                SecurityAlgorithms.HmacSha256));
+
+        return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserDto dto)
+    {
+        var user = await _userService.GetByEmailAsync(dto.Email);
+
+        if (user == null || user.PasswordHash != await _userService.CreatePasswordHash(dto.Password))
+        {
+            return BadRequest(new { message = "Username or password is incorrect" });
+        }
+        
+        var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Email) };
+        var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromDays(1)),
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),
+                SecurityAlgorithms.HmacSha256));
+
+        return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+    }
+}
