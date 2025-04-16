c18b0d476efe8083ccdc69923926dd88c9bbd7f7
prrogramistro <r.grigorev1502@gmail.com>
2025-04-16 23:37:26 +0300

    almost done notify

--- LocalNotificationsDemo/Platforms/Android/BackService.cs
+++ LocalNotificationsDemo/Platforms/Android/BackService.cs
@@ -1,7 +1,12 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Nfc;
 using Android.OS;
+using Android.Util;
 using AndroidX.Core.App;
+using Java.Lang;
+using Java.Util.Logging;
+using Microsoft.Maui.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +23,61 @@ public class ForegroundServiceDemo : global::Android.App.Service
         return null;
     }
 
+    private readonly INotificationManagerService _notificationManagerService;
+
+    private static bool IsStarted { get; set; }
+
+    public ForegroundServiceDemo()
+    {
+        _notificationManagerService = MauiApplication.Current.Services.GetService<INotificationManagerService>();
+        _notificationManagerService.NotificationReceived += (sender, eventArgs) =>
+        {
+            var eventData = (NotificationEventArgs)eventArgs;
+            ShowNotification(eventData.Title, eventData.Message);
+        };
+    }
+
+    void ShowNotification(string title, string message)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            var msg = new Label()
+            {
+                Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
+            };
+        });
+    }
 
     public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
     {
-        return StartCommandResult.NotSticky;
+        _notificationManagerService.SendNotification("adwad", "dawdwad");
+
+        if (intent.Action.Equals(Constants.ACTION_START_SERVICE))
+        {
+            if (IsStarted)
+            {
+                Log.Info("wdwa", "OnStartCommand: Restarting the timer.");
+                _notificationManagerService.SendNotification("adwad", "dawdwad");
+            }
+            else
+            {
+                Log.Info("wdwa", "OnStartCommand: Restarting the timer.");
+                IsStarted = true;
+            }
+        }
+        else if (intent.Action.Equals(Constants.ACTION_STOP_SERVICE))
+        {
+            Log.Info("wdwa", "OnStartCommand: Restarting the timer.");
+            StopForeground(StopForegroundFlags.Remove);
+            StopSelf();
+
+        }
+        else if (intent.Action.Equals(Constants.ACTION_RESTART_TIMER))
+        {
+            Log.Info("wdwa", "OnStartCommand: Restarting the timer.");
+
+        }
+
+        return StartCommandResult.Sticky;
     }
 }
